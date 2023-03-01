e4fbafcee67b0b2baf6dc7c37a9ac2147e067fdc
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-01 17:42:48 -0500

    Update worker.py

--- Worker/worker.py
+++ Worker/worker.py
@@ -1,18 +1,11 @@
 import mysql.connector
-import os
-# Get the database connection information from environment variables
-db_host = os.environ.get('DB_HOST')
-db_port = os.environ.get('DB_PORT')
-db_user = os.environ.get('DB_USER')
-db_password = os.environ.get('DB_PASSWORD')
-db_database = os.environ.get('DB_DATABASE')
 
 mydb = mysql.connector.connect(
-  host=db_host,
-  user=db_user,
-  password=db_password,
-  port=db_port,
-  database=db_database
+  host="docker.mikec123-149382.cloud-edu.emulab.net",
+  user="user",
+  password="123",
+  port="3306",
+  database="database"
 )
 
 print(mydb)
