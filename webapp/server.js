a4f888082ea60d7866efd0ddae3b7ef2ffc88732
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-02 10:33:28 -0500

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = 'localhost';
+const hostname = 'docker.mikec123-149438.cloud-edu.emulab.net';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
