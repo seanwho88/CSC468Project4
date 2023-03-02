918541260b7b6f18d05774be597df21866e0f405
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-02 09:14:06 -0500

    Create worker.py

---
+++ worker/worker.py
@@ -0,0 +1,11 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="docker.mikec123-149382.cloud-edu.emulab.net",
+  user="user",
+  password="123",
+  port="3306",
+  database="database"
+)
+
+print(mydb)
