7f76a05b2de4fe20ff0e088eee51d4ea45302f6d
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-02 12:39:37 -0500

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -1,11 +1,19 @@
 import mysql.connector
+import time
 
-mydb = mysql.connector.connect(
-  host="hostname",
-  user="user",
-  password="123",
-  port="3306",
-  database="database"
-)
+connected = False
+while not connected:
+    try:
+        mydb = mysql.connector.connect(
+            host="hostname",
+            user="user",
+            password="123",
+            port="3306",
+            database="database"
+        )
+        connected = True
+    except mysql.connector.Error as err:
+        print("Failed to connect to database:", err)
+        time.sleep(5)
 
-print(mydb)
+print("Connected to database:", mydb)
