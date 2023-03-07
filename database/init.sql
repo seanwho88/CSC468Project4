79da26b0d3794a13bca7d31bd9de5f8b1a40c720
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-07 11:05:35 -0500

    Update init.sql

--- database/init.sql
+++ database/init.sql
@@ -9,3 +9,8 @@ CREATE TABLE users (
   locationID INT NOT NULL,
   PRIMARY KEY (id)
 );
+
+INSERT INTO users (Username, SpotifyID, Latitude, Longitude, CurrentSong, CurrentArtist, locationID)
+VALUES ('user1', 'spotifyid1', 40.7128, -74.0060, 'song1', 'artist1', 1),
+       ('user2', 'spotifyid2', 34.0522, -118.2437, 'song2', 'artist2', 2),
+       ('user3', 'spotifyid3', 51.5074, -0.1278, 'song3', 'artist3', 3);
