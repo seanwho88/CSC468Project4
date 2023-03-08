4625baba55f85beb96b2ce93c5cf12a3ffffb90d
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-08 15:04:46 -0500

    Update app.js

--- webapp/app.js
+++ webapp/app.js
@@ -7,13 +7,5 @@ var pool = mysql.createPool({
     database: 'database'
 });
 
-// Use the pool to execute SQL queries
-pool.getConnection(function (error, connection) {
-    if (error) throw error;
-    connection.query('SELECT * FROM users', function (error, results, fields) {
-        if (error) throw error;
-        console.log(results);
-        connection.release();
-        pool.end();
-    });
-});
+module.exports = pool;
+
