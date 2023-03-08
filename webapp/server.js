26c66ab31b815b33935068b6ac289947507b5e5b
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-08 15:03:56 -0500

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -1,14 +1,71 @@
 const http = require('http');
+const pool = require('./app');
 
-const hn = 'hostname';
+const hostname = 'hostname';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, world!\n');
-});
+function getUsers(callback) {
+  pool.getConnection((error, connection) => {
+    if (error) throw error;
 
-server.listen(port, () => {
-  console.log(`Server running at http://${hn}:${port}/`);
+    connection.query('SELECT * FROM users', (error, results, fields) => {
+      if (error) throw error;
+
+      connection.release();
+      callback(results);
+    });
+  });
+}
+
+getUsers((results) => {
+  console.log(results);
+
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    const data = JSON.stringify(results);
+
+    const html = `
+      <html>
+        <head>
+          <title>Users</title>
+        </head>
+        <body>
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Spotify ID</th>
+                <th>Latitude</th>
+                <th>Longitude</th>
+                <th>Current Song</th>
+                <th>Current Artist</th>
+                <th>Location ID</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+          <script>
+            const data = ${data};
+            const tbody = document.querySelector('tbody');
+            data.forEach(user => {
+              const tr = document.createElement('tr');
+              tr.innerHTML = \`<td>\${user.id}</td><td>\${user.Username}</td><td>\${user.SpotifyID}</td><td>\${user.Latitude}</td><td>\${user.Longitude}</td><td>\${user.CurrentSong}</td><td>\${user.CurrentArtist}</td><td>\${user.locationID}</td>\`;
+              tbody.appendChild(tr);
+            });
+          </script>
+        </body>
+      </html>
+    `;
+
+    res.end(html);
+  });
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
 });
