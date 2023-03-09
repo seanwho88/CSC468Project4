d56ccffe0c3660c14809fc59e98b38949e55be12
seanwho88 <seanwho88@gmail.com>
2023-03-09 09:31:26 -0500

    Copied new spotify directory

---
+++ worker_spotify/worker.py
@@ -0,0 +1,26 @@
+import mysql.connector
+import time
+
+connected = False
+while not connected:
+    try:
+        mydb = mysql.connector.connect(
+            host="hostname",
+            user="user",
+            password="123",
+            port="3306",
+            database="database"
+        )
+        connected = True
+    except mysql.connector.Error as err:
+        print("Failed to connect to database:", err)
+        time.sleep(5)
+
+print("Connected to database:", mydb)
+
+endTime = time.time() + 15
+while True:
+  if(endTime<time.time()):
+    print("Time is: " + str(time.time()))
+    endTime = time.time() + 15
+
