7605939c439d0366de07cc375d4c7ff9ff52d742
DRockwell0 <123646335+DRockwell0@users.noreply.github.com>
2023-03-23 09:36:41 -0400

    This should be good
    sorry if it is bad

--- database/init.sql
+++ database/init.sql
@@ -6,11 +6,16 @@ CREATE TABLE users (
   Longitude FLOAT(10,6) NOT NULL,
   CurrentSong VARCHAR(255),
   CurrentArtist VARCHAR(255),
-  locationID INT NOT NULL,
+  locationOld INT NOT NULL,
+  locationNew INT,
+  FOREIGN KEY (locationID) REFERENCES location(locationID), 
   PRIMARY KEY (id)
 );
 
-INSERT INTO users (Username, SpotifyID, Latitude, Longitude, CurrentSong, CurrentArtist, locationID)
-VALUES ('user1', 'spotifyid1', 40.7128, -74.0060, 'song1', 'artist1', 1),
-       ('user2', 'spotifyid2', 34.0522, -118.2437, 'song2', 'artist2', 2),
-       ('user3', 'spotifyid3', 51.5074, -0.1278, 'song3', 'artist3', 3);
+CREATE TABLE location (
+  locationID INT PRIMARY KEY,
+  longitude FLOAT(10,6) NOT NULL,
+  latitude  FLOAT(10,6) NOT NULL
+);
+
+
