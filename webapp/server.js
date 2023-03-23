73df5b18509b3cb767f42eebdee43ffde4c95924
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-03-23 15:52:45 -0400

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -17,55 +17,73 @@ function getUsers(callback) {
   });
 }
 
-getUsers((results) => {
-  console.log(results);
+function startServer() {
+  getUsers((results) => {
+    console.log(results);
 
-  const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    const data = JSON.stringify(results);
+    const server = http.createServer((req, res) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      
+      const data = JSON.stringify(results);
 
-    const html = `
-      <html>
-        <head>
-          <title>Users</title>
-        </head>
-        <body>
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Spotify ID</th>
-                <th>Latitude</th>
-                <th>Longitude</th>
-                <th>Current Song</th>
-                <th>Current Artist</th>
-                <th>Location ID</th>
-              </tr>
-            </thead>
-            <tbody>
-            </tbody>
-          </table>
-          <script>
-            const data = ${data};
-            const tbody = document.querySelector('tbody');
-            data.forEach(user => {
-              const tr = document.createElement('tr');
-              tr.innerHTML = \`<td>\${user.id}</td><td>\${user.Username}</td><td>\${user.SpotifyID}</td><td>\${user.Latitude}</td><td>\${user.Longitude}</td><td>\${user.CurrentSong}</td><td>\${user.CurrentArtist}</td><td>\${user.locationID}</td>\`;
-              tbody.appendChild(tr);
-            });
-          </script>
-        </body>
-      </html>
-    `;
+      const html = `
+        <html>
+          <head>
+            <title>Users</title>
+          </head>
+          <body>
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Username</th>
+                  <th>Spotify ID</th>
+                  <th>Latitude</th>
+                  <th>Longitude</th>
+                  <th>Current Song</th>
+                  <th>Current Artist</th>
+                  <th>Location ID</th>
+                </tr>
+              </thead>
+              <tbody>
+              </tbody>
+            </table>
+            <script>
+              const data = ${data};
+              const tbody = document.querySelector('tbody');
+              data.forEach(user => {
+                const tr = document.createElement('tr');
+                tr.innerHTML = \`<td>\${user.id}</td><td>\${user.Username}</td><td>\${user.SpotifyID}</td><td>\${user.Latitude}</td><td>\${user.Longitude}</td><td>\${user.CurrentSong}</td><td>\${user.CurrentArtist}</td><td>\${user.locationID}</td>\`;
+                tbody.appendChild(tr);
+              });
+            </script>
+          </body>
+        </html>
+      `;
 
-    res.end(html);
+      res.end(html);
+    });
+
+    server.listen(port, () => {
+      console.log(`Server running at http://${hn}:${port}/`);
+    });
   });
+}
 
-  server.listen(port, () => {
-    console.log(`Server running at http://${hn}:${port}/`);
+// Keep trying to connect to the database until successful
+function connectToDatabase() {
+  pool.getConnection((error, connection) => {
+    if (error) {
+      console.error('Error connecting to database:', error);
+      setTimeout(connectToDatabase, 5000); // retry after 5 seconds
+    } else {
+      console.log('Connected to database');
+      connection.release();
+      startServer();
+    }
   });
-});
+}
+
+connectToDatabase();
