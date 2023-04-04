01d96cf9a10e57878a234aa68d06fc1020a0820c
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-04 19:46:16 -0400

    Create deploy_spotter.sh

---
+++ deploy_spotter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -x
+namespaceStatus=$(kubectl get namespaces spotter -o json | jq .status.phase -r)
+
+if [ $namespaceStatus == "Active" ]
+then
+    echo "Namespace spotter exists, need to clean up"
+    kubectl delete namespaces spotter
+fi
+
+echo "Creating namespace spotter"
+kubectl create namespace spotter
+ 
+echo "Creating pods"
+kubectl create -f spotter.yaml --namespace spotter
+
+echo "Creating services"
+kubectl create -f spotter-service.yaml --namespace spotter
+
+
+kubectl get pods -n spotter
