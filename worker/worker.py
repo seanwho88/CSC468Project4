f9fa019f56bd26bf240f974a0874a09ea9b75c19
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-04 21:25:25 -0400

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -6,7 +6,7 @@ connected = False
 while not connected:
     try:
         mydb = mysql.connector.connect(
-            host="pcvm547-1.emulab.net",
+            host="mysql",
             user="user",
             password="123",
             port="3306",
