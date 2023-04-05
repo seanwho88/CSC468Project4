b42271adf79d1b2b0dacc5d0007784d90822add4
DRockwell0 <123646335+DRockwell0@users.noreply.github.com>
2023-04-04 20:27:30 -0400

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -6,7 +6,7 @@ connected = False
 while not connected:
     try:
         mydb = mysql.connector.connect(
-            host="hostname",
+            host="pcvm547-1.emulab.net",
             user="user",
             password="123",
             port="3306",
@@ -19,16 +19,50 @@ while not connected:
 
 print("Connected to database:", mydb)
 
+
 # Create a cursor object
-c = conn.cursor()
+c = mydb.cursor()
+
+# Get all users from the users table
+c.execute("SELECT * FROM users")
+users = c.fetchall()
 
 for user in users:
-    if user[7] != user[8]
-        c.execute("UPDATE users SET locationOld = ? WHERE id = ?", (user[8], user[0]))
+    userid = user[1]
+    print(userid)
+    print("Checking user: {}".format(userid))
+
+    # Get the current locationNew value for the user
+    c.execute("SELECT locationNew FROM users WHERE Username = %s", (userid,))
+    result = c.fetchone()
+
+    if result is not None:
+        locationNew = result[0]
+    else:
+        print("Error: locationNew not found for user: {}".format(userid))
+        continue
+
+
+    # Check if locationNew and locationOld match for the user
+    c.execute("SELECT locationOld FROM users WHERE Username = %s", (userid,))
+    result = c.fetchone()
+
+    if result is not None:
+        locationOld = result[0]
+    else:
+        print("Error: locationOld not found for user: {}".format(userid))
+        continue
+
+    if locationNew != locationOld:
+        # Update locationOld to match locationNew for the user
+        c.execute("UPDATE users SET locationOld = %s WHERE Username = %s AND locationOld != locationNew", (locationNew, userid))
+        mydb.commit()
+        print("Updated locationOld for user: {}".format(userid))
+
 
 endTime = time.time() + 15
+
 while True:
   if(endTime<time.time()):
     print("Time is: " + str(time.time()))
     endTime = time.time() + 15
-
