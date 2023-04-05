bfa6462cf99ad2742130f3726027c38dc66b15e1
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-05 16:56:12 -0400

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -6,4 +6,4 @@ name=$(hostname -f)
 
 sed -i "/^ *host: *'mysql'/ s/'mysql'/'${name}'/" ./webapp/app.js
 sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker/worker.py
-sed -i "/^ *host: *'hostname'/ s/'hostname'/'${name}'/" ./webapp/server.js
+sed -i "s/hostname/${name}/g" ./webapp/server.js
