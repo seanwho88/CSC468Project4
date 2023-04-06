005c23dd99e0a754f583585febb0468f07338022
DRockwell0 <123646335+DRockwell0@users.noreply.github.com>
2023-04-06 08:15:14 -0400

    Update init.sql
    removed the hard code dummy data

--- database/init.sql
+++ database/init.sql
@@ -18,12 +18,3 @@ CREATE TABLE users (
   PRIMARY KEY (id)
 );
 
-INSERT INTO location (locationID, longitude, latitude) VALUES
-(1, 34.0522, -118.2437),
-(2, 40.7128, -74.0060),
-(3, 51.5074, -0.1278);
-
-INSERT INTO users (Username, SpotifyID, Latitude, Longitude, CurrentSong, CurrentArtist, locationOld, locationNew) VALUES
-('john_doe', 'abc123', 34.0522, -118.2437, 'Bohemian Rhapsody', 'Queen', 1, 2),
-('jane_smith', 'def456', 40.7128, -74.0060, 'Shape of You', 'Ed Sheeran', 2, 3),
-('bob_johnson', 'ghi789', 51.5074, -0.1278, 'Stairway to Heaven', 'Led Zeppelin', 3, 1);
