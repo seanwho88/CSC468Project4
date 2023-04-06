add77494aeef49b0e7f07bfdb75baf0e1ff8099d
DRockwell0 <123646335+DRockwell0@users.noreply.github.com>
2023-04-05 20:12:52 -0400

    Update data.py
    working dummy data gen

--- worker/data.py
+++ worker/data.py
@@ -4,37 +4,53 @@ import string
 
 # connect to the database
 mydb = mysql.connector.connect(
-  host="hostname",
-  user="user",
-  password="123",
-  port="3306",
-  database="database"
+    host="hostname",
+    user="user",
+    password="123",
+    port="3306",
+    database="database"
 )
 
 # create a cursor object
 mycursor = mydb.cursor()
 
 # create a table (if it doesn't exist)
-#mycursor.execute("CREATE TABLE IF NOT EXISTS employees (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)")
+# mycursor.execute("CREATE TABLE IF NOT EXISTS employees (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), age INT)")
+
+for i in range(11):
+    locid  = (i + 4) #for the 3 hard coded locations
+    loclat = random.uniform(0, 100)
+    loclong = random.uniform(0, 100)
+
+    qry = "INSERT INTO location (locationID, longitude, latitude) VALUES (%s, %s, %s)"
+    val = (locid, loclat, loclong)
+    mycursor.execute(qry, val)
+
+
+mydb.commit()
+
 
 # insert 25 new data sets into the table
+
 for i in range(25):
-    #generate random string
+    # generate random string
     N = 10
-    rstr = ''.join(random.choices(string.ascii_uppercase + string.digits, K=N))
-  
+    rstr = ''.join(random.choices(string.ascii_uppercase + string.digits, k=N))
+
     # generate a random name and age
-    uid = i
-    uname = "user " +str(i+1)
-    spotid = "user " +str(i+1)
-    lat = random.uniform(0, 100)
-    longi = random.uniform(0, 100)
+    uid = (i +4)
+    uname = "user " + str(i + 1)
+    spotid = "user " + str(i + 1)
+    lat = random.uniform(1, 100)
+    longi = random.uniform(1, 100)
+    currsong = rstr
     currart = rstr
-    locid = random.randint(0,10)
-    
+    locNew = random.randint(1, 14)
+    locOld = random.randint(1, 14)
+
     # insert the data into the table
-    sql = "INSERT INTO users (uid, Username, spotifyid, latitude, longitude, CurrentArtist, loccationid) VALUES (%s, %s, %s, %s, %s, %s)"
-    val = (uid, uname, spotid, lat, longi, currart, locid)
+    sql = "INSERT INTO users (id, Username, SpotifyID, Latitude, Longitude, CurrentSong, CurrentArtist, locationOld, locationNew) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (uid, uname, spotid, lat, longi, currart,currsong, locOld, locNew)
     mycursor.execute(sql, val)
 
 # commit the changes to the database
