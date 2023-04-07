d65d7f2cd12227f7cc63969a28933de802f5591e
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-07 16:14:20 -0400

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -1,78 +1,31 @@
-const http = require('http');
-const pool = require('./app');
+const express = require('express');
+const fs = require('fs');
+const pool = require('./db');
+const apiRouter = require('./public/api');
+const expressStatic = require('express-static');
 
-const hn = 'hostname';
-const port = 3000;
+const hn = "hostname"
+const app = express();
+const port = 3001;
 
-function getUsers(callback) {
-  pool.getConnection((error, connection) => {
-    if (error) throw error;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', apiRouter); // use the api router for paths starting with /api
+app.use(expressStatic('public'));
 
-    connection.query('SELECT * FROM users', (error, results, fields) => {
-      if (error) throw error;
-
-      connection.release();
-      callback(results);
-    });
-  });
-}
-
-function startServer() {
+app.get('/', (req, res) => {
   getUsers((results) => {
-    console.log(results);
-
-    const server = http.createServer((req, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'text/html');
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      
-      const data = JSON.stringify(results);
-
-      const html = `
-        <html>
-          <head>
-            <title>Users</title>
-          </head>
-          <body>
-            <table>
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Username</th>
-                  <th>Spotify ID</th>
-                  <th>Latitude</th>
-                  <th>Longitude</th>
-                  <th>Current Song</th>
-                  <th>Current Artist</th>
-                  <th>Location ID</th>
-                </tr>
-              </thead>
-              <tbody>
-              </tbody>
-            </table>
-            <script>
-              const data = ${data};
-              const tbody = document.querySelector('tbody');
-              data.forEach(user => {
-                const tr = document.createElement('tr');
-                tr.innerHTML = \`<td>\${user.id}</td><td>\${user.Username}</td><td>\${user.SpotifyID}</td><td>\${user.Latitude}</td><td>\${user.Longitude}</td><td>\${user.CurrentSong}</td><td>\${user.CurrentArtist}</td><td>\${user.locationID}</td>\`;
-                tbody.appendChild(tr);
-              });
-            </script>
-          </body>
-        </html>
-      `;
-
-      res.end(html);
-    });
-
-    server.listen(port, () => {
-      console.log(`Server running at http://${hn}:${port}/`);
+    fs.readFile(__dirname + '/index.html', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error reading index.html');
+      } else {
+        res.send(data);
+      }
     });
   });
-}
+});
 
-// Keep trying to connect to the database until successful
 function connectToDatabase() {
   pool.getConnection((error, connection) => {
     if (error) {
@@ -81,7 +34,9 @@ function connectToDatabase() {
     } else {
       console.log('Connected to database');
       connection.release();
-      startServer();
+      app.listen(port, () => {
+        console.log(`Server running at http://${hn}:${port}/`);
+      });
     }
   });
 }
