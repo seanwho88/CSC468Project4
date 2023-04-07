ea25a6355262d678cc1223fc1132f2c099f8f907
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-07 19:25:35 -0400

    Update hostname.sh

--- hostname.sh
+++ hostname.sh
@@ -9,4 +9,5 @@ sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker/worker.py
 sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker/data.py
 sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker_spotify/worker.py
 sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker_spotify/data.py
+sed -i "/^ *host *= *\"mysql\"/ s/\"mysql\"/\"${name}\"/" ./worker/locup.py
 sed -i "s/hostname/${name}/g" ./webapp/server.js
