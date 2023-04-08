66ea78b7349c1844a305380c3d908a0c214dfcb7
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-08 03:25:01 -0400

    Update api.js

--- webapp/public/api.js
+++ webapp/public/api.js
@@ -4,90 +4,119 @@ const pool = require('../db');
 const router = express.Router();
 
 function getUsers(callback) {
-  pool.getConnection((error, connection) => {
-    if (error) throw error;
+    pool.getConnection((error, connection) => {
+        if (error) throw error;
 
-    connection.query('SELECT * FROM users', (error, results, fields) => {
-      if (error) throw error;
+        connection.query('SELECT * FROM users', (error, results, fields) => {
+            if (error) throw error;
 
-      connection.release();
-      callback(results);
+            connection.release();
+            callback(results);
+        });
     });
-  });
 }
 
 function saveCurrentlyPlaying(spotifyID, song, artist, callback) {
     pool.getConnection((error, connection) => {
-      if (error) throw error;
-  
-      connection.query(
-        'UPDATE users SET currentSong = ?, currentArtist = ? WHERE SpotifyID = ?',
-        [song, artist, spotifyID],
-        (error, results, fields) => {
-          if (error) throw error;
-  
-          connection.release();
-          console.log(`Updated currently playing song and artist for user ${spotifyID}`);
-          callback();
+        if (error) {
+            if (typeof callback === 'function') {
+                callback(error);
+            }
+            return;
         }
-      );
+
+        connection.query(
+            'UPDATE users SET currentSong = ?, currentArtist = ? WHERE SpotifyID = ?',
+            [song, artist, spotifyID],
+            (error, results, fields) => {
+                if (error) {
+                    console.error('Error:', error);
+                    if (typeof callback === 'function') {
+                        callback(error);
+                    }
+                    return;
+                }
+
+                connection.release();
+                console.log(`Updated currently playing song and artist for user ${spotifyID}`);
+                if (typeof callback === 'function') {
+                    callback(results);
+                }
+            }
+        );
     });
-  }
-  
+}
 
-/* function updateUserLocation(latitude, longitude, callback) {
+function updateUserLocation(latitude, longitude, spotifyID, callback) {
     pool.getConnection((error, connection) => {
-      if (error) throw error;
-      connection.query(`UPDATE users SET Latitude = ${latitude}, Longitude = ${longitude}`, (error, results, fields) => {
-        if (error) throw error;
-  
-        connection.release();
-        console.log(`Updated user location to (${latitude}, ${longitude})`);
-
-        setTimeout(() => {
-            updateUserLocation(latitude,longitude, callback);
-        }, 10000);
-      });
-    });
-  } */
-
-  function updateUserLocation(latitude, longitude, spotifyID, callback) {
-    pool.getConnection((error, connection) => {
-      if (error) throw error;
-  
-      connection.query(
-        'UPDATE users SET Latitude = ?, Longitude = ? WHERE SpotifyID = ?',
-        [latitude, longitude, spotifyID],
-        (error, results, fields) => {
-          if (error) throw error;
-  
-          connection.release();
-          console.log(`Updated user location to (${latitude}, ${longitude}) for user ${spotifyID}`);
-          callback();
+        if (error) {
+            if (typeof callback === 'function') {
+                callback(error);
+            }
+            return;
         }
-      );
+
+        connection.query(
+            'UPDATE users SET Latitude = ?, Longitude = ? WHERE SpotifyID = ?',
+            [latitude, longitude, spotifyID],
+            (error, results, fields) => {
+                if (error) {
+                    console.error('Error:', error);
+                    if (typeof callback === 'function') {
+                        callback(results);
+                    }
+                    return;
+                }
+
+                connection.release();
+                console.log(`Updated user location to (${latitude}, ${longitude}) for user ${spotifyID}`);
+                if (typeof callback === 'function') {
+                    callback(null);
+                }
+            }
+        );
     });
-  }
-  
+}
+
+
+
 
 router.get('/users', (req, res) => {
-  getUsers((results) => {
-    res.json(results);
-  });
+    getUsers((results) => {
+        res.json(results);
+    });
 });
 
 router.post('/updateUserLocation', (req, res) => {
     const { latitude, longitude, spotifyID } = req.body;
-    updateUserLocation(latitude, longitude, spotifyID, () => {
-      res.sendStatus(201);
+    updateUserLocation(latitude, longitude, spotifyID, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error updating user location');
+        } else {
+            res.sendStatus(201);
+        }
     });
-  });
+});
 
-  router.post('/saveCurrentlyPlaying', (req, res) => {
+router.post('/saveCurrentlyPlaying', (req, res) => {
+    const { spotifyID, song, artist } = req.body;
+    saveCurrentlyPlaying(spotifyID, song, artist, (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error saving currently playing song');
+        } else {
+            res.sendStatus(201);
+        }
+    });
+});
+
+
+router.post('/saveCurrentlyPlaying', (req, res) => {
     const { spotifyID, song, artist } = req.body;
     saveCurrentlyPlaying(spotifyID, song, artist, () => {
-      res.sendStatus(201);
+        res.sendStatus(201);
     });
-  });
-  
+});
+
 module.exports = router;
