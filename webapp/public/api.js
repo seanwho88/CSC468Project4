c7eaf09aa6ee73269666687d74247d30aa8ee0ff
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-08 00:52:12 -0400

    Update api.js

--- webapp/public/api.js
+++ webapp/public/api.js
@@ -16,17 +16,28 @@ function getUsers(callback) {
   });
 }
 
-function updateUserLocation(latitude, longitude, callback) {
+function saveCurrentlyPlaying(spotifyID, song, artist, callback) {
     pool.getConnection((error, connection) => {
       if (error) throw error;
   
-     /*  const query = 'INSERT INTO location (latitude, longitude) VALUES (?, ?)';
-      connection.query(query, [latitude, longitude], (error, results, fields) => {
-        if (error) throw error;
+      connection.query(
+        'UPDATE users SET currentSong = ?, currentArtist = ? WHERE SpotifyID = ?',
+        [song, artist, spotifyID],
+        (error, results, fields) => {
+          if (error) throw error;
   
-        connection.release();
-        callback();
-      }); */
+          connection.release();
+          console.log(`Updated currently playing song and artist for user ${spotifyID}`);
+          callback();
+        }
+      );
+    });
+  }
+  
+
+/* function updateUserLocation(latitude, longitude, callback) {
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
       connection.query(`UPDATE users SET Latitude = ${latitude}, Longitude = ${longitude}`, (error, results, fields) => {
         if (error) throw error;
   
@@ -38,7 +49,26 @@ function updateUserLocation(latitude, longitude, callback) {
         }, 10000);
       });
     });
+  } */
+
+  function updateUserLocation(latitude, longitude, spotifyID, callback) {
+    pool.getConnection((error, connection) => {
+      if (error) throw error;
+  
+      connection.query(
+        'UPDATE users SET Latitude = ?, Longitude = ? WHERE SpotifyID = ?',
+        [latitude, longitude, spotifyID],
+        (error, results, fields) => {
+          if (error) throw error;
+  
+          connection.release();
+          console.log(`Updated user location to (${latitude}, ${longitude}) for user ${spotifyID}`);
+          callback();
+        }
+      );
+    });
   }
+  
 
 router.get('/users', (req, res) => {
   getUsers((results) => {
@@ -47,8 +77,15 @@ router.get('/users', (req, res) => {
 });
 
 router.post('/updateUserLocation', (req, res) => {
-    const { latitude, longitude } = req.body;
-    updateUserLocation(latitude, longitude, () => {
+    const { latitude, longitude, spotifyID } = req.body;
+    updateUserLocation(latitude, longitude, spotifyID, () => {
+      res.sendStatus(201);
+    });
+  });
+
+  router.post('/saveCurrentlyPlaying', (req, res) => {
+    const { spotifyID, song, artist } = req.body;
+    saveCurrentlyPlaying(spotifyID, song, artist, () => {
       res.sendStatus(201);
     });
   });
