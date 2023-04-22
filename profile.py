4f6daba9b3d82d502af3b91dade36e958acadbe6
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-22 09:58:39 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -14,7 +14,7 @@ request = pc.makeRequestRSpec()
 
 tourDescription = \
 """
-This profile provides the template for 3 compute nodes with Docker and Kubernetes installed on Ubuntu 18.04
+This profile provides the template for 3 compute nodes with Docker and Kubernetes installed on Ubuntu 20
 """
 
 #
@@ -38,7 +38,7 @@ for i in range(num_nodes):
     node.cores = 4
     node.ram = 4096 
     node.routable_control_ip = "true" 
-    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU18-64-STD"
+    node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU20-64-STD"
     iface = node.addInterface("if" + str(i))
     iface.component_id = "eth1"
     iface.addAddress(pg.IPv4Address(prefixForIP + str(i + 1), "255.255.255.0"))
