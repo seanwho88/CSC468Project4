53edd8ef0b30ce9bbbbc6b225762ba9566d0dee2
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-29 00:55:52 -0400

    Update server.js

--- webapp/server.js
+++ webapp/server.js
@@ -10,13 +10,13 @@ const axios = require('axios');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-const hn = "hostname";
-//const HEAD_NODE_HOSTNAME = process.env.HEAD_NODE_HOSTNAME;
+//const hn = "hostname";
+const HEAD_NODE_HOSTNAME = process.env.HEAD_NODE_HOSTNAME;
 const app = express();
 const port = 3000;
 const SPOTIFY_CLIENT_ID = '35a5e3f642214b238a5015aa91e9d9f8';
 const SPOTIFY_CLIENT_SECRET = '185026c7c8b646a382279a4ceae0bd38';
-const SPOTIFY_REDIRECT_URI = `http://${hn}:3000/callback`;
+const SPOTIFY_REDIRECT_URI = `http://${HEAD_NODE_HOSTNAME}:3000/callback`;
 //console.log(`HEAD_NODE_HOSTNAME: ${HEAD_NODE_HOSTNAME}`);
 
 
@@ -194,7 +194,7 @@ function connectToDatabase() {
       console.log('Connected to database');
       connection.release();
       app.listen(port, () => {
-        console.log(`Server running at ${hn}:3000/`);
+        console.log(`Server running at ${HEAD_NODE_HOSTNAME}:3000/`);
 	//console.log("redirect uri: " + SPOTIFY_REDIRECT_URI);
 	
       });
