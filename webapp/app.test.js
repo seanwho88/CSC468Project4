0d4c469a6fd8c7d3e27990c5883ac4415d6d812b
mikec1233 <99062550+mikec1233@users.noreply.github.com>
2023-04-28 21:50:20 -0400

    Update app.test.js

--- webapp/app.test.js
+++ webapp/app.test.js
@@ -3,7 +3,7 @@ const { add } = require('./public/api');
 describe('Utility functions', () => {
   describe('add', () => {
     it('should return the sum of two numbers', () => {
-      const result = add.add(1, 2);
+      const result = add(1, 2);
       expect(result).toBe(3);
     });
   });
